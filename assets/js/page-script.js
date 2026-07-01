4d4d6b697309b9854ad32473585abecca86ae3b4
Seriki Walter Yakub <sandraisom@icloud.com>
2026-07-01 17:48:42 +0100

    Create page-script.js

---
+++ assets/js/page-script.js
@@ -0,0 +1,15 @@
+// page-script.js
+
+let messenger = document.getElementById("from-page-script");
+
+messenger.addEventListener("click", messageContentScript);
+
+function messageContentScript() {
+  window.postMessage(
+    {
+      direction: "from-page-script",
+      message: "Message from the page",
+    },
+    "*",
+  );
+}
