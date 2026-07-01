59ee5f48472bc3d4b83020f47afb7aac8cca3a6c
Seriki Walter Yakub <sandraisom@icloud.com>
2026-07-01 17:54:28 +0100

    Update page-script.js

--- assets/js/page-script.js
+++ assets/js/page-script.js
@@ -1,15 +1,15 @@
 // page-script.js
 
-let messenger = document.getElementById("from-page-script");
+// add a new element to the DOM
+let p = document.createElement("p");
+p.textContent = "This paragraph was added by a page script.";
+p.setAttribute("id", "page-script-para");
+document.body.appendChild(p);
 
-messenger.addEventListener("click", messageContentScript);
+// define a new property on the window
+window.foo = "This global variable was added by a page script";
 
-function messageContentScript() {
-  window.postMessage(
-    {
-      direction: "from-page-script",
-      message: "Message from the page",
-    },
-    "*",
-  );
-}
+// redefine the built-in window.confirm() function
+window.confirm = () => {
+  alert("The page script has also redefined 'confirm'");
+};
